c0b1b3a6784bb828d3ae5eaf8ce84d222c2003cc
Mosegrodd <magnusl@sticos.no>
2019-09-23 19:53:21 +0200

    init

---
+++ TestProsjekt/TestProsjekt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestProsjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
